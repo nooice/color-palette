6b803c378b229e1587f7a586368fbbdcd03f965b
nooice <imagetskanky@MacBook-Pro-2.local>
2019-05-27 22:51:31 -0600

    added build folder

---
+++ client/build/precache-manifest.171ac825038edf5aa0ec770a52ec4178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab852d98c2230a3a8b69",
+    "url": "/static/css/main.e8f89809.chunk.css"
+  },
+  {
+    "revision": "ab852d98c2230a3a8b69",
+    "url": "/static/js/main.425f82aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2af577618e396c66ce82",
+    "url": "/static/css/2.56880415.chunk.css"
+  },
+  {
+    "revision": "2af577618e396c66ce82",
+    "url": "/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "abc1e1eeb35cd6b1880acfa713e04864",
+    "url": "/index.html"
+  }
+];
